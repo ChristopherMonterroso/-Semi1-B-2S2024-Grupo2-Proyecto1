c4a0f41c5d57ac52c7eba6b4441a25249b0308c1
Estuardo Ruiz <zruiz89@gmail.com>
2024-10-20 18:32:27 -0600

    Agregado servicio para calendario

--- e-student-app/app/services/config.js
+++ e-student-app/app/services/config.js
@@ -2,5 +2,6 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const ENDPOINTS = {
   REGISTER: '/users/register',
-  LOGIN: '/auth/login'
+  LOGIN: '/auth/login',
+  SCHEDULES: '/user/shedules'
 };
